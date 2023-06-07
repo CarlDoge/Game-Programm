cbe380e2713a35032ea1a6351e704b553bc7fc79
CarlDoge <135844763+CarlDoge@users.noreply.github.com>
2023-06-07 21:48:29 +0800

    Add files via upload

---
+++ MouseLook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    public float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
